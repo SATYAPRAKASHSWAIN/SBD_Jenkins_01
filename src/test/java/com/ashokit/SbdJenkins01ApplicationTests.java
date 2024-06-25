33683c344702f0e2e6745f04e86ce30d51f14cde
SATYAPRAKASHSWAIN <satyaprakashswain1998@gmail.com>
2024-06-25 21:36:17 +0530

    faluire commited

--- src/test/java/com/ashokit/SbdJenkins01ApplicationTests.java
+++ src/test/java/com/ashokit/SbdJenkins01ApplicationTests.java
@@ -15,7 +15,7 @@ class SbdJenkins01ApplicationTests {
 	public static Logger logger=LoggerFactory.getLogger(SbdJenkins01Application.class);
 
 	@Test
-	public void contextLoads() {
+	public  contextLoads() {
 		logger.info("Test case executing ..............");
 		assertEquals(true, true);
 	}
